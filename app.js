937be93f6cb8c5a21fbaf89ab13a7f9be5e8e2dc
linh.nguyen5 <linh.nguyen5@CL-LINHNGUYEN5>
2018-11-15 13:47:11 +0700

    move folders

--- app.js
+++ app.js
@@ -2,20 +2,10 @@ var studentRoute = require('./studentRouteHelper');
 var express = require('express'),
     app = express(),
     port = process.env.port || 3000;
-
-
-
-
-// var spendingCollection = db.collection("Spending");
-
-// //uset get() to get entire documents in a colletion
-// spendingCollection.get().then((snapshot)=>{
-//   snapshot.forEach(doc=>{
-//     console.log(`id: ${doc.id} with data ${doc.data()}`);
-//   })
-// });
-
+    
 studentRoute(app);
+app.use(express.static(__dirname + '/static'));
+
 app.listen(port, ()=>{
   console.log(`Server running on port ${port}`);
 });
